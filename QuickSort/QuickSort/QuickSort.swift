77c6c9df62d048fdb00b0de2f9270cf3bba16d00
yangjun <yangjun@touzila.com>
2015-11-16 17:49:05 +0800

    快速排序完成

--- QuickSort/QuickSort/QuickSort.swift
+++ QuickSort/QuickSort/QuickSort.swift
@@ -17,17 +17,9 @@ class QuickSort: NSObject {
     /// 数组
     var list = Array<Int>()
     
-    // MARK: 全部排序
-    func sort() {
+    // MARK: - 快速排序
+    func quickSort() {
         self.quickSort(0, end: self.list.count-1)
-    }
-    
-    // MARK: 部分排序，包含first、end
-    func sort(var first:Int, var end:Int) {
-        // 安全机制
-        first = first < 0 ? 0:first
-        end = end >= self.list.count ? self.list.count-1:end
-        self.quickSort(first, end: end)
     }
     
     // MARK: 分解
@@ -35,29 +27,80 @@ class QuickSort: NSObject {
         if first < end {
             // 获取中位点
             let location = self.partition(first, end: end)
-            self.quickSort(first, end: location)
+            self.quickSort(first, end: location-1)
             self.quickSort(location+1, end: end)
         }
     }
     
     // MARK: 排序核心算法
-    private func partition(var first:Int, end:Int) ->Int {
-        let x: Int = self.list[end]
+    private func partition(first:Int, end:Int) ->Int {
+        var index = first - 1
         for(var i = first; i < end; i++) {
-            if self.list[i] <= x {
-                first++
-                self.exchange(i, first)
+            if self.list[i] < self.list[end] {
+                index++
+                self.exchange(i, index)
             }
         }
-        self.exchange(first, end)
-        return first
+        index++
+        self.exchange(index, end)
+        return index
     }
     
     // MARK: 交换数据
     private func exchange(p:Int, _ r:Int){
+        if p == r {
+            return
+        }
         let temp = self.list[p]
         self.list[p] = self.list[r]
         self.list[r] = temp
+    }
+    
+    // MARK: - 随机快速排序
+    func radomQuickSort() {
+        self.radomQuickSort(0, end: self.list.count-1)
+    }
+    
+    // MARK: 开始随机排序
+    private func radomQuickSort(first:Int, end:Int) {
+        if first < end {
+            // 获取中位点
+            let location = self.radomPartition(first, end: end)
+            self.radomQuickSort(first, end: location-1)
+            self.radomQuickSort(location+1, end: end)
+        }
+    }
+    
+    // MARK: 获取划分的位置
+    private func radomPartition(first:Int, end:Int) ->Int {
+        // 随机一个数放到尾部，主要防止遇到最坏排序情况，即数组为倒序
+        // 获取first-end的随机数
+        let random = arc4random_uniform(UInt32(end-first+1))
+        let index = Int(random) + first
+        self.exchange(index, end);
+        return self.partition(first, end: end)
+    }
+    
+    // MARK: - 多线程快速排序
+    func threadQuickSort() {
+         self.threadQuickSort(0, end: self.list.count-1)
+    }
+    
+    // MARK: 开始多线程快速排序
+    private func threadQuickSort(first:Int, end:Int) {
+        // 在随机排序的基础上启用多线程排序
+        if first < end {
+            // 获取中位点
+            let location = self.radomPartition(first, end: end)
+            // 分到多个线程排序，并发
+            let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+            dispatch_async(queue, { () -> Void in
+                self.threadQuickSort(first, end: location-1)
+            })
+            dispatch_async(queue, { () -> Void in
+                self.threadQuickSort(location+1, end: end)
+            })
+        }
     }
     
 }
--- QuickSort/QuickSort/main.swift
+++ QuickSort/QuickSort/main.swift
@@ -11,25 +11,45 @@
 
 import Foundation
 
+// 快速排序测试
 let quickSort = QuickSort()
 quickSort.list.append(0)
 for(var i = 10; i > 0; i--){
     quickSort.list.append(i)
 }
-print("原始数组:\(quickSort.list)")
-quickSort.sort(3, end: 7)
-print("部分排序3-7:\(quickSort.list)")
-quickSort.sort()
-print("全部排序:\(quickSort.list)")
+quickSort.quickSort()
+print("快速排序:\(quickSort.list)")
 
-let formatter = NSDateFormatter()
-formatter.dateFormat = "hh:mm:ss"
-quickSort.list.removeAll()
-for(var i = 100; i > 0; i--){
-    quickSort.list.append(i)
+
+// 速度测试，使用最坏情况测试，也就是原数组为倒序
+let count = 10000 // 要排序的元素个数
+var source = Array<Int>()
+for(var i = count; i > 0; i--){
+    source.append(i)
 }
-print("快速排序100,0000个数，开始:\(formatter.stringFromDate(NSDate()))")
+print("快排速度测试,总数：\(count)")
 
-quickSort.sort()
-print("快速排序100,0000个数，结束:\(formatter.stringFromDate(NSDate()))")
+// 快速排序
+var date = NSDate()
+quickSort.list = source
+quickSort.quickSort()
+print("快速排序耗时:\(-date.timeIntervalSinceNow)")
+
+// 随机快速排序
+date = NSDate()
+quickSort.list = source
+quickSort.radomQuickSort()
+print("随机快速排序耗时:\(-date.timeIntervalSinceDate(NSDate()))")
+
+// 多线程快速排序
+date = NSDate()
+quickSort.list = source
+quickSort.threadQuickSort()
+print("多线程快速排序耗时:\(-date.timeIntervalSinceNow)")
+
+
+
+
+
+
 
