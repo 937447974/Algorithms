422ef741dbfa19aca26af938386a036f32640705
yangjun <yangjun@touzila.com>
2015-11-19 16:41:35 +0800

    二叉树，结点元素开发

--- BinarySearchTree/BinarySearchTree/YJBinarySearchTree.swift
+++ BinarySearchTree/BinarySearchTree/YJBinarySearchTree.swift
@@ -2,12 +2,53 @@
 //  YJBinarySearchTree.swift
 //  BinarySearchTree
 //
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
 //  Created by yangjun on 15/11/18.
 //  Copyright © 2015年 阳君. All rights reserved.
 //
 
 import Cocoa
 
+/// 二叉搜索树
 class YJBinarySearchTree {
 
+    /// 根结点
+    private var rootItem:YJTreeItem?
+    
+    // MARK: - 中序遍历
+    /// 中序遍历
+    ///
+    /// - returns: void
+    func inorderTreeWalk() {
+        self.inorderTreeWalk(self.rootItem)
+    }
+    
+    func inorderTreeWalk(item:YJTreeItem?) {
+        if item != nil {
+            self.inorderTreeWalk(item!.left)
+            print(item!.key)
+            self.inorderTreeWalk(item!.right)
+        }
+    }
+    
+    // MARK: - 查找
+    /// 查找
+    ///
+    /// - parameter key : 要查找的key
+    ///
+    /// - returns: YJTreeItem
+    func treeSearch(key:Int) -> YJTreeItem? {
+        var item = self.rootItem
+        while item != nil || key != item?.key {
+            if item?.key > key { // 当前结点大于key时搜索左子结点
+                item = item?.left
+            } else { // 否则进入右子结点
+                item = item?.right
+            }
+        }
+        return item
+    }
+   
 }
---
+++ BinarySearchTree/BinarySearchTree/YJTreeItem.swift
@@ -0,0 +1,24 @@
+//
+//  YJTreeItem.swift
+//  BinarySearchTree
+//
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
+//  Created by yangjun on 15/11/19.
+//  Copyright © 2015年 阳君. All rights reserved.
+//
+
+import Cocoa
+
+/// 树结点
+class YJTreeItem {
+
+    /// 关键字
+    var key:Int!
+    /// 左子结点
+    var left:YJTreeItem?
+    /// 右子结点
+    var right:YJTreeItem?
+    
+}
